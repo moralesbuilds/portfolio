5a5d9d783c0a869ab09dc1b95f979058d79dc9dd
Luis Morales <luis@modellus.app>
2026-09-08 21:05:38 -0500

    change homepage to server component

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
-import { useTranslations } from "next-intl";
+import { getTranslations } from 'next-intl/server';
 
-export default function Home() {
-  const t = useTranslations("home");
+export default async function Home() {
+  const t = await getTranslations("home");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">{t("title")}</h1>
