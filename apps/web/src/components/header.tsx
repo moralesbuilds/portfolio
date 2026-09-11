ef5e8f51b30b3b71dfd8c0230155857a34a07946
moralesbuilds <luis@moralesbuilds.dev>
2026-09-10 20:14:50 -0500

    comment header parts to aid code inspections

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -22,10 +22,12 @@ export default function Header({ brandLabel, blogLabel, contactLabel, openMenuLa
       <nav className="bg-surface border-b border-gray-200 font-mono">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
+            {/* Brand name */}
             <div className="shrink-0 flex items-center">
               <Link className="text-text-primary flex-none font-semibold text-xl focus:outline-hidden focus:opacity-80" href="#" aria-label={brandLabel}>{brandLabel}</Link>
             </div>
 
+            {/* Desktop links */}
             <div className="hidden sm:flex sm:items-center sm:space-x-8">
               <div className="flex space-x-8 h-full">
                 <Link href="#" className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">{blogLabel}</Link>
@@ -41,6 +43,7 @@ export default function Header({ brandLabel, blogLabel, contactLabel, openMenuLa
               </div>
             </div>
 
+            {/* Mobile menu button */}
             <div className="-mr-2 flex items-center sm:hidden">
               <button id="mobile-menu-button" type="button" onClick={onMobileMenuButtonClicked} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none" aria-controls="mobile-menu" aria-expanded="false">
                 <span className="sr-only">{openMenuLabel}</span>
@@ -52,6 +55,7 @@ export default function Header({ brandLabel, blogLabel, contactLabel, openMenuLa
           </div>
         </div>
 
+        {/* Mobile menu links */}
         <div className={mobileMenuOpened ? 'sm:hidden' : 'hidden sm:hidden'} id="mobile-menu">
           <div className="pt-2 pb-3 space-y-1">
             <Link href="#" className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 font-medium">{blogLabel}</Link>
