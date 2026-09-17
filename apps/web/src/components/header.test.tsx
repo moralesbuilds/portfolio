8f8edfc372de2b87e6ce84c22df307cc626c14b6
moralesbuilds <luis@moralesbuilds.dev>
2026-09-17 16:19:53 -0500

    added selected page in header

--- apps/web/src/components/header.test.tsx
+++ apps/web/src/components/header.test.tsx
@@ -1,7 +1,16 @@
 import { NextIntlClientProvider } from "next-intl";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { Header } from "./header";
 import { render, screen } from "@testing-library/react";
+import { useSelectedLayoutSegments } from "next/navigation";
+
+vi.mock('next/navigation', async (importActual) => {
+  const actual = await importActual<typeof import('next/navigation')>();
+  return {
+    ...actual,
+    useSelectedLayoutSegments: vi.fn()
+  };
+});
 
 function renderHeader() {
   render(
@@ -33,5 +42,41 @@ describe("Header (unit)", () => {
 
     const github = screen.getAllByTestId("github-link");
     expect(github[0]).toHaveAttribute("href", "https://github.com/test/test");
+  });
+
+  test("show no menu as selected", () => {
+    vi.mocked(useSelectedLayoutSegments).mockReturnValue([]);
+    
+    renderHeader();
+
+    expect(screen.getByTestId("blog-link")).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByTestId("mobile-blog-link")).toHaveAttribute("aria-selected", "false");
+
+    expect(screen.getByTestId("contact-link")).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByTestId("mobile-contact-link")).toHaveAttribute("aria-selected", "false");
+  });
+
+  test("show the blog menu as selected", () => {
+    vi.mocked(useSelectedLayoutSegments).mockReturnValue(["blog"]);
+    
+    renderHeader();
+
+    expect(screen.getByTestId("blog-link")).toHaveAttribute("aria-selected", "true");
+    expect(screen.getByTestId("mobile-blog-link")).toHaveAttribute("aria-selected", "true");
+
+    expect(screen.getByTestId("contact-link")).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByTestId("mobile-contact-link")).toHaveAttribute("aria-selected", "false");
+  });
+
+  test("show the blog menu as selected", () => {
+    vi.mocked(useSelectedLayoutSegments).mockReturnValue(["contact"]);
+    
+    renderHeader();
+
+    expect(screen.getByTestId("blog-link")).toHaveAttribute("aria-selected", "false");
+    expect(screen.getByTestId("mobile-blog-link")).toHaveAttribute("aria-selected", "false");
+
+    expect(screen.getByTestId("contact-link")).toHaveAttribute("aria-selected", "true");
+    expect(screen.getByTestId("mobile-contact-link")).toHaveAttribute("aria-selected", "true");
   });
 });
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,8 +1,45 @@
 "use client"
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "../i18n/navigation";
 import { GitHubIcon, MenuIcon } from "./icons";
+import { useSelectedLayoutSegments } from "next/navigation";
+
+const COMMON_DESKTOP_STYLE = "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+const SELECTED_DESKTOP_STYLE = `border-indigo-500 text-gray-900 ${COMMON_DESKTOP_STYLE}`;
+const UNSELECTED_DESKTOP_STYLE = `border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700  ${COMMON_DESKTOP_STYLE}`;
+
+const COMMON_MOBILE_STYLE = "block pl-3 pr-4 py-2 border-l-4 font-medium";
+const SELECTED_MOBILE_STYLE = `bg-indigo-50 border-indigo-500 text-indigo-700 ${COMMON_MOBILE_STYLE}`;
+const UNSELECTED_MOBILE_STYLE = `border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 ${COMMON_MOBILE_STYLE}`;
+
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string;
+  label: string;
+  selected: boolean;
+};
+
+function DesktopLink({ href, label, selected, ...rest }: LinkProps) {
+  return (
+    <Link
+      href={href}
+      className={selected ? SELECTED_DESKTOP_STYLE : UNSELECTED_DESKTOP_STYLE}
+      aria-selected={selected}
+      {...rest}
+    >{label}</Link>
+  );
+}
+
+function MobileLink({ href, label, selected, ...rest }: LinkProps) {
+  return (
+    <Link
+      href={href}
+      className={selected ? SELECTED_MOBILE_STYLE : UNSELECTED_MOBILE_STYLE}
+      aria-selected={selected}
+      {...rest}
+    >{label}</Link>
+  );
+}
 
 type HeaderProps = {
   brandLabel: string;
@@ -17,8 +54,12 @@ type HeaderProps = {
 export function Header({ brandLabel, blogLabel, contactLabel, openMenuLabel, githubLabel, githubUrl }: HeaderProps) {
   const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
   const onMobileMenuButtonClicked = () => setMobileMenuOpened((prevValue) => !prevValue);
+  const segments = useSelectedLayoutSegments();
 
-  const github = (
+  const isBlogSelected = segments?.[0] === "blog";
+  const isContactSelected = segments?.[0] === "contact";
+
+  const githubLink = (
     <a
       href={githubUrl}
       target="_blank"
@@ -49,26 +90,18 @@ export function Header({ brandLabel, blogLabel, contactLabel, openMenuLabel, git
             {/* Desktop links */}
             <div className="hidden sm:flex sm:items-center sm:space-x-8">
               <div className="flex space-x-8 h-full">
-                <Link
-                  href="/blog"
-                  className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  data-testid="blog-link"
-                >{blogLabel}</Link>
-                <Link
-                  href="#"
-                  className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  data-testid="contact-link"
-                >{contactLabel}</Link>
+                <DesktopLink href="/blog" label={blogLabel} selected={isBlogSelected} data-testid="blog-link" />
+                <DesktopLink href="#" label={contactLabel} selected={isContactSelected} data-testid="contact-link" />
               </div>
 
               <div className="flex items-center space-x-4 border-l border-gray-200 pl-6">
-                {github}
+                {githubLink}
               </div>
             </div>
 
             {/* Mobile menu button */}
             <div className="-mr-2 flex items-center sm:hidden gap-2" data-testid="mobile-panel">
-              {github}
+              {githubLink}
               <button
                 id="mobile-menu-button"
                 type="button"
@@ -88,16 +121,8 @@ export function Header({ brandLabel, blogLabel, contactLabel, openMenuLabel, git
         {/* Mobile menu links */}
         <div className={mobileMenuOpened ? 'sm:hidden' : 'hidden sm:hidden'} id="mobile-menu">
           <div className="pt-2 pb-3 space-y-1">
-            <Link
-              href="#"
-              className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 font-medium"
-              data-testid="mobile-blog-link"
-            >{blogLabel}</Link>
-            <Link
-              href="#"
-              className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 font-medium"
-              data-testid="mobile-contact-link"
-            >{contactLabel}</Link>
+            <MobileLink href="/blog" label={blogLabel} selected={isBlogSelected} data-testid="mobile-blog-link" />
+            <MobileLink href="#" label={contactLabel} selected={isContactSelected} data-testid="mobile-contact-link" />
           </div>
         </div>
       </nav>
